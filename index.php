e70b6feb386f6ccc906b5ac9639336a19e18a4b8
Elian <cris.ufam2012@gmail.com>
2019-09-02 23:58:12 -0400

    Criacao da pagina incial do login

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<form method="post" action="logar.php">
+	<table width="200" border="0">
+		<tr>
+			<td align="right">Login</td>
+			<td><input type="text" name="cpf"></td>
+		</tr>
+		<tr>
+			<td align="right">Senha</td>
+			<td><input type="text" name="senha"></td>
+		</tr>
+		<tr>
+			<td align="right"></td>
+			<td><input type="submit" value="Entrar"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
